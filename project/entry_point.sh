9f039bf449613c2f35ba6230eca9825d2b45449e
HannahWu04 <151092141+HannahWu04@users.noreply.github.com>
2025-03-20 19:30:21 -0400

    New folder format, Updated feature selection (data eda and preparation)

---
+++ project/entry_point.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Rscript required/requirements.txt
+Rscript features/Features_1.ipynb
+Rscript features/Features_2.ipynb
+
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
+# Rscript models/________.ipynb
